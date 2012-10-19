92e9213a4d8392006186feb8768e907350cba89f
Patrick Robertson <critical.wing@gmail.com>
2012-10-19 21:08:58 +0100

    BookBatchPorcessor Completed and Tested

---
+++ CAF-PR-Lib/src/test/resources/ie/cit/patrick/test-data.sql
@@ -0,0 +1,19 @@
+INSERT INTO book (id,title,author,publisher,publication_date,isbn,available) 
+VALUES ('3421','Raspberry Pi User Guide', 'Eben Upton', 'John Wiley & Sons', '2012-09-14', '111846446X', false);
+INSERT INTO book (id,title,author,publisher,publication_date,isbn,available) 
+VALUES ('1236','HTML & CSS: Design and Build Web Sites', 'Jon Duckett', 'John Wiley & Sons', '2011-11-18', '123456789B',true);
+INSERT INTO book (title,author,publisher,publication_date,isbn) 
+VALUES ('Python Programming for the Absolute Beginner', 'Mike Dawson', 'Course Technology PTR', '2010-01-30', '1435455002');
+INSERT INTO book (title,author,publisher,publication_date,isbn) 
+VALUES ('Digital Marketing Strategy Implementation and Practice', 'Dave Chaffey', 'Pearson', '2012-07-12', '0273746103');
+INSERT INTO book (id,title,author,publisher,publication_date,isbn) 
+VALUES ('4454','Dancing at Lughnasa', 'A N Other', 'Faber and Faber', '2012-07-12', '0571144799');
+
+INSERT INTO Member (name,address1,address2,town,contact_number,balance) 
+VALUES ('Bertie Ahern', 'Big House', 'Rich Street', 'Dublin', '018001231235','1500.00');
+INSERT INTO Member (name,address1,address2,town,contact_number,balance) 
+VALUES ('Chris Stephenson', 'New House','Stephen Street', 'Boyle', '0331234567', '0.00');
+INSERT INTO Member (name,address1,address2,town,contact_number,balance) 
+VALUES ('Peter Smith', '18', 'St Johns Close', 'Macroom', '02212344567', '10.00');
+INSERT INTO Member (name,address1,address2,town,contact_number,balance) 
+VALUES ('Vera Saunders', '24A', 'Elysian', 'Cork', '0211234667', '20.00');
