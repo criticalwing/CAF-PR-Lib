a0b80d03c2eedd66d6843638c9afc5dedbf4bcba
Patrick Robertson <critical.wing@gmail.com>
2012-10-29 09:38:44 +0000

    Added AspectJ and Log4J to system & tested

---
+++ CAF-PR-Lib/src/main/java/ie/cit/patrick/service/LoanReturnLogger.java
@@ -0,0 +1,55 @@
+package ie.cit.patrick.service;
+
+import ie.cit.patrick.service.impl.LibraryServiceImpl;
+
+import org.apache.log4j.Logger;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
+@Aspect
+public class LoanReturnLogger {
+
+	Logger logger = Logger.getLogger(LibraryServiceImpl.class.getName());
+	
+	LoanReturnLogger(){
+	}
+	
+	@Before("execution(* LibraryService+.returnBook(..))")
+	public void trackReturnStart(JoinPoint point){
+		String memberId = point.getArgs()[0].toString();
+		String bookId = point.getArgs()[1].toString();		
+		logger.info("Member:" + memberId + " will attempt to return book with Id:" + bookId);
+		System.out.print("Member:" + memberId + " will attempt to return book with Id:" + bookId + "\n");
+	}
+	
+	@After("execution(* LibraryService+.returnBook(..))")
+	public void trackReturnEnd(JoinPoint point){
+		String memberId = point.getArgs()[0].toString();
+		String bookId = point.getArgs()[1].toString();		
+		logger.info("Member:" + memberId + " has returned book book with Id:" + bookId);
+		System.out.print("Member:" + memberId + " has returned book book with Id:" + bookId + "\n\n");
+	}
+	
+	@Before("execution(* LibraryService+.loanBook(..))")
+	public void trackLoanStart(JoinPoint point){
+		String memberId = point.getArgs()[0].toString();
+		String bookId = point.getArgs()[1].toString();		
+		logger.info("Member:" + memberId + " will attempt to loan book with Id:" + bookId);
+		System.out.print("Member:" + memberId + " will attempt to loan book with Id:" + bookId + "\n");
+	}
+	
+	@After("execution(* LibraryService+.loanBook(..))")
+	public void trackLoanEnd(JoinPoint point){
+		String memberId = point.getArgs()[0].toString();
+		String bookId = point.getArgs()[1].toString();		
+		logger.info("Member:" + memberId + " has loaned book with Id:" + bookId);
+		System.out.print("Member:" + memberId + " has loaned book with Id:" + bookId + "\n\n");
+	}
+	
+	
+	
+}
+
